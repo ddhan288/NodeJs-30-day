1de92933c1ad322582df5d1be00c7194a1e881bc
ddhan288 <ddhan288@gmail.com>
2024-02-07 02:56:59 +0530

    modified day6.js

---
+++ day6.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+
+const app = express ();
+
+function greetHandler (req , res ){
+    
+    const name = req.query.name;
+    if (name){
+        res.send(`Hello, ${name} !`);
+    }else {
+        res.send ('Hello, Guest !');
+    }
+    
+    
+    
+}
+app.get ('/greet', greetHandler);
+const port = process.env.PORT || 3000
+app.listen(port , ()=> console.log('server is runing on port 3000'))
