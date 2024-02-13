18ca24f2fa2fcc72f25083cb4642ce38421a9722
ddhan288 <ddhan288@gmail.com>
2024-02-13 22:12:00 +0530

    modified day12.js

--- day12.js
+++ day12.js
@@ -1,12 +1,13 @@
+// Problem Statement: Implement a rate-limiting middleware for an Express application. 
+// The middleware should limit the number of requests from a single IP address to a specified rate, 
+// and return a 429 Too Many Requests status if the limit is exceeded.
 const express = require ('express');
 const app = express ();
-
 const reqCounts = new map();
 
 function rateLimitMiddleware(req, res, next) {
     const maxRequestsPerMinute = 3;
     const ipAddress = req.ip;
-
     if (!reqCounts.has(ipAddress)) {
         reqCounts.set(ipAddress, 1);
     }else {
@@ -21,5 +22,10 @@ function rateLimitMiddleware(req, res, next) {
 
 app.use(rateLimitMiddleware);
 app.get('/', (req, res) => {
-    res.json({'message': "hello Deepak"});
+    res.json({'message': "hello alisha"});
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log (`Server is running on ${port}`);
 });
